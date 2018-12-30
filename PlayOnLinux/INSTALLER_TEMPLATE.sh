1aa8a3a5388b846a0087f3132c604f0ef859bfce
Kreyren <KostWarCZE@RiXotStudio.cz>
2018-12-30 18:08:14 +0000

    Update INSTALLER_TEMPLATE.sh

--- PlayOnLinux/INSTALLER_TEMPLATE.sh
+++ PlayOnLinux/INSTALLER_TEMPLATE.sh
@@ -32,6 +32,20 @@ else
 fi
 
 
+## REFERENCES
+# Introduction - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_1:_Getting_to_know_Bash
+# Basic Functions - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_2:_Basic_Functions
+# Variables - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_3:_Variables
+# Conditions - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_4:_Conditions
+# Wine - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_5:_Wine
+# Filesystem - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_6:_The_Filesystem
+# Installation Media - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_7:_Installation_Media
+# Real Script - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_8:_My_First_Real_Script
+# Standartization - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_9:_Standardization
+# Script translation - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_10:_Script_Translation
+# List of functions - http://wiki.playonlinux.com/index.php/Scripting_-_Chapter_11:_List_of_Functions
+# 
+
 ## FUNCTIONS - https://github.com/PlayOnLinux/POL-POM-4/tree/df9e090b4db5b7a41f72145877cc541b908c319b/lib
 
 ### DEBUG FUNCTIONS - https://github.com/PlayOnLinux/POL-POM-4/blob/df9e090b4db5b7a41f72145877cc541b908c319b/lib/debug.lib 
